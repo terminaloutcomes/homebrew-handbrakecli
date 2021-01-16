fdc05a99516bbd111bffc82f6ca4ae50014bc71a
James Hodgkinson <james@terminaloutcomes.com>
2021-01-16 15:50:21 +1000

    fixing

--- handbrakecli.rb
+++ handbrakecli.rb
@@ -6,7 +6,7 @@ class HandBrakeCLI < Formula
   
   url "https://github.com/HandBrake/HandBrake/releases/download/#{version}/HandBrakeCLI-#{version}.dmg"
   sha256 "833b6cd98a2e43121d1de2ba0d7d9b4dbf76244967d3cdbaa2fc3cd1a3500f0a"
-  name "HandBrakeCLI"
+  #name "HandBrakeCLI"
   def install
     bin.install "HandBrakeCLI"
   end
