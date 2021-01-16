87e4e947552bfb4ef5ba1549b3bf413effbaa3fa
James Hodgkinson <james@terminaloutcomes.com>
2021-01-16 16:05:04 +1000

    fixing things

--- handbrakecli.rb
+++ handbrakecli.rb
@@ -4,8 +4,6 @@ class Handbrakecli < Formula
   version "1.3.3"
   license "GPLv2"
 
-  depends_on :mp4v2
-  
   url "https://github.com/HandBrake/HandBrake/releases/download/#{version}/HandBrakeCLI-#{version}.dmg"
   sha256 "833b6cd98a2e43121d1de2ba0d7d9b4dbf76244967d3cdbaa2fc3cd1a3500f0a"
   #name "HandBrakeCLI"
