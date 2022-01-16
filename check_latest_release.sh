ef04ffd1272956f4dad98a6b3909c75edf8cdd65
James Hodgkinson <james@terminaloutcomes.com>
2022-01-16 10:33:32 +1000

    fixing up check script

--- check_latest_release.sh
+++ check_latest_release.sh
@@ -12,21 +12,40 @@ get_latest_release() {
 
 SPECFILE=$(find "$(pwd)" -type f -name '*.rb' | head -n1)
 
+if [ -z "${SPECFILE}" ]; then
+    echo "Failed to find specfile, bailing!"
+    exit 1
+fi
+
 # grab the update url from the spec file
 URL=$(grep -E homepage "${SPECFILE}" | awk '{print $NF}' | tr -d '"')
+if [ -z "${URL}" ]; then
+    echo "Failed to find check URL, bailing!"
+    exit 1
+fi
+
+
 # pull the latest version
 LATEST=$(get_latest_release "${URL}" )
+if [ -z "${LATEST}" ]; then
+    echo "Failed to find latest version, bailing!"
+    exit 1
+fi
 
-DOWNLOAD_URL=$(grep -E 'url \"http.*' handbrakecli.rb | awk '{print $NF}' | tr -d '"' | sed -E "s/#{version}/${LATEST_VERSION}/g")
+# pull the download url from the spec file and update it
+DOWNLOAD_URL=$(grep -E 'url \"http.*' "${SPECFILE}" | awk '{print $NF}' | tr -d '"' | sed -E "s/#{version}/${LATEST}/g")
+if [ -z "${DOWNLOAD_URL}" ]; then
+    echo "Failed to find download URL, bailing!"
+    exit 1
+fi
 
+# calculate the shasum based on the file
 FILEHASH=$(curl -L --silent "${DOWNLOAD_URL}" | shasum -a 256 | awk '{print $1}')
+if [ -z "${FILEHASH}" ]; then
+    echo "Couldn't get file hash, bailing"
+    exit 1
+fi
 
-# if [ -z "${LATEST}" ]; then
-#     echo "Couldn't find release" >&2
-#     exit 1
-# fi
-
-# echo "Found latest for ${REPO}: ${LATEST}" >&2
 # updates the file
 find "$(pwd)" -maxdepth 1 -type f -name '*.rb' -exec sed -i "" -E "s/version \\\".*/version \"${LATEST}\"/g" "{}" \;
 find "$(pwd)" -maxdepth 1 -type f -name '*.rb' -exec sed -i "" -E "s/sha256 \\\".*/sha256 \"${FILEHASH}\"/g" "{}" \;
