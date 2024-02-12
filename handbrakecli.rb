9fbc0a62e2df668ebcf579195e7cedec5bad650b
Version bump <james@terminaloutcomes.com>
2024-02-12 05:15:58 +0000

    Updated version automagically for ref: ${PR_HEAD_REF}

--- handbrakecli.rb
+++ handbrakecli.rb
@@ -1,13 +1,13 @@
 class Handbrakecli < Formula
   desc "HandBrake CLI"
   homepage "https://api.github.com/repos/HandBrake/HandBrake/releases/latest"
-  version "1.7.2"
+  version "1.7.3"
   license "GPLv2"
 
   # https://handbrake.fr/rotation.php?file=HandBrakeCLI-1.5.1.dmg
   # https://github.com/HandBrake/HandBrake/releases/download/1.5.1/HandBrakeCLI-1.5.1.dmg
   url "https://github.com/HandBrake/HandBrake/releases/download/#{version}/HandBrakeCLI-#{version}.dmg"
-  sha256 "4211f704e8fa39968a85b23b880ee5bfaf7b575dca1fecfc94788fe449d38666"
+  sha256 "aa6039c6e7a7bcecff333540459feee3d257b60b41a63f3562912be6b72ab4af"
   def install
     bin.install "HandBrakeCLI"
   end
